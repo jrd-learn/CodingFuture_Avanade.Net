dccdc94d80759b1bcbdf4205d22783afe4077e08
jrd-learn <jrd.learn@gmail.com>
2024-03-21 16:15:24 -0300

    Adicionado Desafios de Código
    Adicionado os dois primeiros desafios de código:
    1 - Condicionalmente Rico: Tomadas de Decisão no Código
    2 - Lista de Itens

---
+++ Desafios_de_Codigo/Condicionalmente_Rico__Tomadas_de_Decisao_no_Codigo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Condicionalmente_Rico__Tomadas_de_Decisao_no_Codigo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int saldoTotal = int.Parse(Console.ReadLine());
+
+
+            int valorSaque = int.Parse(Console.ReadLine());
+
+            //TODO: Criar as condições necessárias para impressão da saída, vide tabela de exemplos.
+            //Resolução direta
+            if (saldoTotal >= valorSaque)
+            {
+                Console.WriteLine($"Saque realizado com sucesso. Novo saldo: {saldoTotal - valorSaque}");
+            }
+            else
+            {
+                Console.WriteLine($"Saldo insuficiente. Saque nao realizado!");
+            }
+
+            //Resolução com a Função string Saque(int,int)
+            // Console.WriteLine(Saque(saldoTotal, valorSaque));
+        }
+
+        /*
+            static string Saque(int saldoTotal, int valorSaque)
+            {
+            if (saldoTotal >= valorSaque)
+            {
+                return $"Saque realizado com sucesso. Novo saldo: {saldoTotal - valorSaque}";
+            }
+            else
+            {
+                return $"Saldo insuficiente. Saque nao realizado!";
+            }
+        */
+    }
+}
---
+++ Desafios_de_Codigo/Lista_de_Itens/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lista_de_Itens
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lista para armazenar os itens
+            List<string> itens = new List<string>();
+
+            // TODO: Solicite os itens ao usuário
+            for (int i = 0; i < 3; i++)
+            {
+                itens.Add(i.ToString(Console.ReadLine()));
+            }
+
+            // Exibe a lista de itens
+            Console.WriteLine("Lista de itens:");
+            foreach (string item in itens)
+            {
+                Console.WriteLine($"- {item}");
+            }
+        }
+    }
+}
