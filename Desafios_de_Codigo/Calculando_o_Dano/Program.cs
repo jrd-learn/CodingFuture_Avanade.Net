756e3851f9fdcafecfacef88a9ff69d841ee4f07
jrd-learn <jrd.learn@gmail.com>
2024-03-21 18:15:49 -0300

    Adicionado o desafio Calculando o Dano.

---
+++ Desafios_de_Codigo/Calculando_o_Dano/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Calculando_o_Dano
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int CalcularDano(int ataque, int defesa)
+            {
+                //TODO: Implemente a função calcular dano
+                if (ataque >= defesa)
+                {
+                    return ataque - defesa;
+                }
+
+                return 0;
+
+            }
+
+            static void Main(string[] args)
+            {
+                int ataque = int.Parse(Console.ReadLine());
+
+                int defesa = int.Parse(Console.ReadLine());
+
+                int danoCausado = CalcularDano(ataque, defesa);
+                Console.WriteLine("O dano causado pelo ataque foi: " + danoCausado);
+            }
+        }
+    }
+}
