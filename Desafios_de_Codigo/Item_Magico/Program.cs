7f0d46f9351268a4b2f5ddc01559b28f3957b3a4
jrd-learn <jrd.learn@gmail.com>
2024-03-21 21:23:54 -0300

    Adicionado a resolução do Desafio: Item Mágico

---
+++ Desafios_de_Codigo/Item_Magico/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Item_Magico
+{
+    public class ItemMagico
+    {
+        //TODO:  Crie a classe ItemMagico com os atributos nome, descrição e poder
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int Poder { get; set; }
+
+        public ItemMagico(string nome, string descricao, int poder)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Poder = poder;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Recebe a entrada do usuário
+
+            string nome = Console.ReadLine();
+
+
+            string descricao = Console.ReadLine();
+
+
+            int poder = int.Parse(Console.ReadLine());
+
+            // Cria o item mágico
+            ItemMagico item = new ItemMagico(nome, descricao, poder);
+
+            // Imprime o item criado
+            Console.WriteLine($"Item: {item.Nome}\nDescrição: {item.Descricao}\nPoder: {item.Poder}");
+        }
+    }
+}
